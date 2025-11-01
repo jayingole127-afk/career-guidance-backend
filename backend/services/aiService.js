4b54f992e59d8e4357189a89462aa645f37e6aa6
jayingole127-afk <jayingole127@gmail.com>
2025-11-01 17:47:13 +0530

    some changes

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -1,4 +1,7 @@
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
